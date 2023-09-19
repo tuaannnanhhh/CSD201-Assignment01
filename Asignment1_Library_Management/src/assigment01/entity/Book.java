8f194c8f41d807635fcb66e5f00d35c23fe3ccbb
QuanHMHE173490 <120399038+wabt@users.noreply.github.com>
2023-09-19 22:42:11 +0700

    Book.java

--- Asignment1_Library_Management/src/assigment01/entity/Book.java
+++ Asignment1_Library_Management/src/assigment01/entity/Book.java
@@ -23,5 +23,46 @@ public class Book {
         this.lended = 0;
         this.price = price; 
     }
+
+    public String getBcode() {
+        return bcode;
+    }
+
+    public void setBcode(String bcode) {
+        this.bcode = bcode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getLended() {
+        return lended;
+    }
+
+    public void setLended(int lended) {
+        this.lended = lended;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     
 }
