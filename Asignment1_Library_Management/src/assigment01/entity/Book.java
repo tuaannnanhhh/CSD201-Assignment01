820d3d80c75213620292ecad86cec7a703d64d2b
QuanHMHE173490 <120399038+wabt@users.noreply.github.com>
2023-09-19 22:20:10 +0700

    Book.java

--- Asignment1_Library_Management/src/assigment01/entity/Book.java
+++ Asignment1_Library_Management/src/assigment01/entity/Book.java
@@ -10,5 +10,18 @@ package assigment01.entity;
  * @author nghin
  */
 public class Book {
+    private String bcode;
+    private String title;
+    private int quantity;
+    private int lended;
+    private double price;
+
+    public Book ( String bcode, String title, int quantity, double price) {
+        this.bcode = bcode;
+        this. title = title;
+        this.quantity = quantity;
+        this.lended = 0;
+        this.price = price; 
+    }
     
 }
